8d37eb32edbaef37dd605964b7dceb15b002bc7e
A1QKT <taquangkhoi1232002@gmail.com>
2022-05-22 09:21:36 +0700

    socket component

--- src/components/sockets/socketComponent.tsx
+++ src/components/sockets/socketComponent.tsx
@@ -1,39 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
 const SocketComponent = () => {
     const socketRound  = io("http://localhost:4000/round");
-    socketRound.on("connection", () => console.log(socketRound.id));
-    const [stateRound, setStateRound] : any[]= useState(null);
-
-    socketRound.on("send-round", (round) => {
-        setStateRound(round);
-    });
+    const [stateRound, setStateRound] = useState<Array<any>>([]);
+    useEffect(() => {
+        socketRound.on("connection", () => console.log(socketRound.id));
+        socketRound.on("send-round", (round) => {
+            setStateRound((prevState) => [...prevState, round]);
+        })
+    }, [])
     
-    setInterval(() => {socketRound.emit("update-round", "ping")}, 60000);
-    socketRound.on("send-update-round", (newRound) => {
-        let oldState = stateRound;
-        console.log(stateRound);
-        let round: any = []
-        round.concat(oldState);
-        round.push(newRound);
-        setStateRound(round);
-    })
     return (
         <div>
             <div>this websocket</div>
             <div>
-            {stateRound}
-            {/* {stateRound.map((element: any) => {
-                return (
-                    <div>
-                        {element}
-                    </div>
-                )
-            })} */}
+            {stateRound.map((element: any) => {return <>{element}</>})}
             </div>
         </div> 
     )
-} 
+}
 
 export default SocketComponent;
